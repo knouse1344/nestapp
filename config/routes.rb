fdcecfb1830a04c9d4fe77b7c95b476c91fe4164
Bryan <bknouse@u.rochester.edu>
2014-01-21 12:24:46 -0500

    updated all pages, about 50% there

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Nestapp::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'welcome#index'
+
+  get 'because/iamanengineer'
+  get 'because/iamadesigner'
+  get 'because/iamabusinessman'
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
