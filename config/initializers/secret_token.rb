20167c7dc781327903e0fdd443cc8100a1527c3a
Bryan <bknouse@u.rochester.edu>
2014-01-20 21:45:47 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nestapp::Application.config.secret_key_base = 'e567309908b26ccc5051b72865a9b5ee8ec950a4d7dee43ce9e2c6d33bab786f69d9b6e2934f7f91ddb89113ada6e22c1744a2908eee06f105cd01a971835448'
